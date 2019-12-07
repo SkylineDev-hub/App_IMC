eb2686470439aa4643dbd05e06a7ee65490bb459
AltissiaDev <alveseric42@gmail.com>
2019-12-07 12:58:57 -0300

    Adicionadas melhorias na Interface do usuário

--- src/components/inform.js
+++ src/components/inform.js
@@ -4,9 +4,9 @@ import { View, Text, StyleSheet} from "react-native";
 export default function Inform(props,stade){
     const styles= StyleSheet.create({
         styleText : {
-            color : "#fff",
-            fontWeight: "bold",
-            fontSize : 25
+            color : "#d3d3d3",
+            fontSize : 25,
+            fontWeight: 'bold'
         },
         view : {
             alignSelf: "stretch",
@@ -20,7 +20,7 @@ export default function Inform(props,stade){
     return(
     <View style = {styles.view}>
             <Text style = {styles.styleText}>{props.text}</Text>
-            <Text style = {styles.styleText}>Indice IMC : {props.imc}</Text>
+            
     </View>
     );
 }
